633abe17c6b35d8e24ee4d86734109610432824c
pushprajjaiswal <pushprajapp12@gmail.com>
2024-05-30 00:20:59 +0530

    Feat: Modified controller for not implemented error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const{PORT} = require('./config/server.config');
+
 const bodyParser = require('body-parser');
+// const BaseError = require('./errors/base.error');
 const apiRouter = require('./routes');
+const errorHandler = require('./utils/errorHandler');
 
 
 const app = express();
@@ -9,6 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended : true}));
 app.use(bodyParser.text());
+
 
 // If any request comes and route starts with /api, we map it to apiRouter
 app.use('/api', apiRouter);
@@ -18,6 +22,10 @@ app.get('/ping',(req, res) =>{
     return res.json({message: 'Problem Service is alive'});
 });
 
+//last middleware if any error comes
+app.use(errorHandler);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+
 });
